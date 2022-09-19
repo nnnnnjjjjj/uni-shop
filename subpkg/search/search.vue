199d5e94d982dedae72d9562fccf4c47c29270ad
zyt <zyt@example.com>
2022-09-20 00:18:55 +0800

    update

--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -1,6 +1,8 @@
 <template>
 	<view>
-		search
+		<view class="search-box">
+			<uni-search-bar @input="input" :radius="100" cancelButton="none"></uni-search-bar>
+		</view>
 	</view>
 </template>
 
@@ -10,10 +12,19 @@
 			return {
 				
 			};
+		},
+		methods:{
+			input(e) {
+				console.log(e)
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
-
+	.search-box {
+		position: sticky;
+		top: 0;
+		z-index: 999;
+	}
 </style>
