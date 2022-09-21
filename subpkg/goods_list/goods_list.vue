76e92127e608312d71594a045ddd6b4154f72fcc
paco.zhu <paco.zhu@marcpoint.com>
2022-09-21 20:27:39 +0800

    update

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,23 @@
 <template>
 	<view>
-		goods_list
+		<block v-for="(goods, i) in goodsList" :key="i">
+			<view class="goods-item">
+			
+				<view class="goods-item-left">
+					<image :src="goods.goods_small_logo" class="goods-pic">
+					</image>
+				</view>
+				
+				<view class="goods-item-right">
+					<view class="goods-name">{{goods.goods_name}}</view>
+					<view class="goods-info-box">
+						<view class="goods-price">
+							￥{{goods.goods_price}}
+						</view>
+					</view>
+				</view>
+			</view>
+		</block>
 	</view>
 </template>
 
@@ -8,15 +25,64 @@
 	export default {
 		data() {
 			return {
-				
+				queryObj: {
+					query: '',
+					cid: '',
+					pagenum: 1,
+					pageSize: 10
+				},
+				goodsList: [],
+				total: 0
 			}
 		},
-		methods: {
+		onLoad(options) {
+			this.queryObj.query = options.query || ''
+			this.queryObj.cid = options.cid || ''
 			
+			this.getGoodsList()
+		},
+		methods: {
+			async getGoodsList() {
+				const {data: res} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				console.log(res)
+				if (res.meta.status !== 200) return uni.$showMsg()
+				
+				this.goodsList = res.message.goods
+				this.total = res.message.total 
+			}
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+	.goods-item {
+		display: flex;
+		padding: 10px 5px;
+		border-bottom: 1px solid #f0f0f0;
+		
+		.goods-item-left {
+			.goods-pic {
+				width: 100px;
+				height: 100px;
+				display: block;
+			}
+		}
+		
+		.goods-item-right {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			
+			.goods-name {
+				font-size: 13px;
+			}
+			
+			.goods-info-box {
+				.goods-price {
+					color: #C00000;
+					font-size: 16px;
+				}
+			}
+		}
+	}
 </style>
