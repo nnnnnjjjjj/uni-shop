fc3165904dc4808cb1352cb917395001a7340cea
zyt <zyt@example.com>
2022-09-22 00:23:25 +0800

    update

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,61 @@
+<template>
+	<view class="goods-item">
+		<view class="goods-item-left">
+			<image :src="goods.goods_small_logo" class="goods-pic">
+			</image>
+		</view>
+		
+		<view class="goods-item-right">
+			<view class="goods-name">{{goods.goods_name}}</view>
+			<view class="goods-info-box">
+				<view class="goods-price">
+					￥{{goods.goods_price}}
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-goods",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.goods-item {
+		display: flex;
+		padding: 10px 5px;
+		border-bottom: 1px solid #f0f0f0;
+		
+		.goods-item-left {
+			.goods-pic {
+				width: 100px;
+				height: 100px;
+				display: block;
+			}
+		}
+		
+		.goods-item-right {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			
+			.goods-name {
+				font-size: 13px;
+			}
+			
+			.goods-info-box {
+				.goods-price {
+					color: #C00000;
+					font-size: 16px;
+				}
+			}
+		}
+	}
+</style>
