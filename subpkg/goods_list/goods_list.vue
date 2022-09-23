bc323358c9a0983d27cd2cd1ecf929fc5c144451
zyt <zyt@example.com>
2022-09-24 00:57:29 +0800

    update

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,8 +1,8 @@
 <template>
 	<view>
-		<block v-for="(goods, i) in goodsList" :key="i">
+		<view v-for="(goods, i) in goodsList" :key="i" @click="gotoDetail(goods)">
 			<my-goods :goods="goods"></my-goods>
-		</block>
+		</view>
 	</view>
 </template>
 
@@ -35,8 +35,15 @@
 			
 			this.queryObj.pagenum += 1
 			this.getGoodsList()
+		},
+		onPullDownRefresh() {
+			// 重置关键数据
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isloading = false
+			this.goodsList = []
 			
-			
+			this.getGoodsList(() => uni.stopPullDownRefresh())
 		},
 		onLoad(options) {
 			this.queryObj.query = options.query || ''
@@ -45,14 +52,20 @@
 			this.getGoodsList()
 		},
 		methods: {
-			async getGoodsList() {
+			async getGoodsList(cb) {
 				this.isloading = true
 				const {data: res} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
 				this.isloading = false
+				cb && cb()
 				if (res.meta.status !== 200) return uni.$showMsg()
 				
 				this.goodsList = [...this.goodsList, ...res.message.goods]
 				this.total = res.message.total 
+			},
+			gotoDetail(item) {
+				uni.navigateTo({
+					url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+				})
 			}
 		}
 	}
